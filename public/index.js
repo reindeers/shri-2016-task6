df385b5e91c358cd1671fce9602e9cc866a148da
sever <reindeers@yandex.ru>
2016-08-08 19:57:03 +0300

    init eslint

--- public/index.js
+++ public/index.js
@@ -1,27 +1,21 @@
- function post(url, data) {
-  return new Promise(function(resolve, reject) {
-    var req = new XMLHttpRequest();
-    req.open("POST", url, data);
-
-    req.onload = function() {
-      if (req.status == 200) {
-        resolve(req.response);
-      }
-      else {
-        reject(Error(req.statusText));
-      }
+function post (url, data) {
+  return new Promise(function (resolve, reject) {
+    let req = new XMLHttpRequest();
+    req.open('POST', url, data);
+    req.onload = function () {
+      if (req.status === 200) resolve(req.response);
+      else reject(Error(req.statusText));
     };
-
-    req.onerror = function() {
-      reject(Error("Network Error"));
+    req.onerror = function () {
+      reject(Error('Network Error'));
     };
     req.send();
   });
 };
 
 let data = { width: screen.width, height: screen.height };
-post("/logger", data).then(function(response) {
-  console.log("Success!", response);
-}, function(error) {
-  console.error("Failed!", error);
+post('/logger', data).then(function (response) {
+  console.log('Success!', response);
+}, function (error) {
+  console.error('Failed!', error);
 });
